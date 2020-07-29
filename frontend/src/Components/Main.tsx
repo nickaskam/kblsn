e1bb43534556323c4bb27b48006953ea614c1e17
Nick Askam <asknickfilms@gmail.com>
2020-07-29 16:33:34 -0500

    building shell of front end

---
+++ frontend/src/Components/Main.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class Main extends Component<{}> {
+  render() {
+    return (
+      <div>
+        <h1>KBLSN</h1>
+        <p>Welcome to KBLSN Sports Network!</p>
+      </div>
+    );
+  }
+}
